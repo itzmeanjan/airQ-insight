f05a3e5844f1bc5457ae4a939b56fcd810a8f9f8
Anjan Roy <anjanroy@yandex.com>
2019-12-15 09:40:51 +0530

    added project dependencies

--- airQInsight/__init__.py
+++ airQInsight/__init__.py
@@ -4,5 +4,5 @@
     A tool designed to power exploratory data analysis, on airQ collected Air Quality Indicator Data.
 '''
 
-__version__ = '0.2.2'
+__version__ = '0.2.4'
 __author__ = 'Anjan Roy<anjanroy@yandex.com>'
