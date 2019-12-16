950659c6c2c406e8863451fe4929d9aee49b2d14
Anjan Roy <anjanroy@yandex.com>
2019-12-16 13:02:01 +0530

    ready for release of 0.2.5

--- airQInsight/__init__.py
+++ airQInsight/__init__.py
@@ -2,7 +2,10 @@
 
 '''
     A tool designed to power exploratory data analysis, on airQ collected Air Quality Indicator Data.
+
+    Source : https://github.com/itzmeanjan/airQ-insight
+    Companion Repo ( of airQ ) : https://github.com/itzmeanjan/airQ
 '''
 
-__version__ = '0.2.4'
+__version__ = '0.2.5'
 __author__ = 'Anjan Roy<anjanroy@yandex.com>'
